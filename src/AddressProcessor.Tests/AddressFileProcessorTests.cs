5ed443cee6c1ce3e1d2da9608ddbaef86cebe145
Chris Monney <chris.monney@gmail.com>
2017-01-17 23:14:35 +0000

    Made sure the existing test was working before starting refactoring

--- src/AddressProcessor.Tests/AddressFileProcessorTests.cs
+++ src/AddressProcessor.Tests/AddressFileProcessorTests.cs
@@ -1,4 +1,6 @@
-﻿using AddressProcessing.Address;
+﻿using System.IO;
+using System.Reflection;
+using AddressProcessing.Address;
 using AddressProcessing.Address.v1;
 using NUnit.Framework;
 
@@ -9,18 +11,22 @@ namespace AddressProcessing.Tests
     {
         private FakeMailShotService _fakeMailShotService;
         private const string TestInputFile = @"test_data\contacts.csv";
+        private string _testInputFilePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
 
         [SetUp]
         public void SetUp()
         {
+            _testInputFilePath = Path.Combine(_testInputFilePath, TestInputFile);
             _fakeMailShotService = new FakeMailShotService();
+       
         }
 
         [Test]
         public void Should_send_mail_using_mailshot_service()
         {
             var processor = new AddressFileProcessor(_fakeMailShotService);
-            processor.Process(TestInputFile);
+            processor.Process(_testInputFilePath);
 
             Assert.That(_fakeMailShotService.Counter, Is.EqualTo(229));
         }
